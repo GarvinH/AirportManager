df4889563a34a0802a5151a26a3ccaad42883bb4
AlbertQuon <albertq2001@hotmail.com>
2019-05-04 15:14:01 -0400

    Update SortBTreeNode.java

--- Trees/SortBTreeNode.java
+++ Trees/SortBTreeNode.java
@@ -1,48 +1,114 @@
-package Trees;
+/* [Flight.java]
+ * Contains flight information
+ * Albert Quon
+ * 04/30/2019
+ */
 
-class SortBTreeNode<E> {
-    private E item;
-    private SortBTreeNode left, right;
-    SortBTreeNode(E item, SortBTreeNode left, SortBTreeNode right){
-        this.item = item;
-        this.left = left;
-        this.right = right;
+import java.util.Date;
+
+public class Flight implements Comparable<Flight>{
+    private String airline, location, name;
+    private Date date;
+    private boolean arrival;
+    private String status;
+
+    /**
+     *
+     * @param airline
+     * @param name
+     * @param arrival
+     * @param location
+     * @param date
+     */
+    Flight(String airline, String name, boolean arrival, String location, Date date, String status){
+        this.airline = airline;
+        this.arrival = arrival;
+        this.location = location;
+        this.date = date;
+        this.name = name;
+        this.status = status;
     }
 
-    SortBTreeNode(E item){
-        this.item = item;
-        this.left = null;
-        this.right = null;
+    /**
+     *
+     * @return
+     */
+    public String getAirline() {
+        return this.airline;
     }
 
-    public E getItem() {
-        return item;
+    /**
+     *
+     * @return
+     */
+    public String getStatus() {
+        return this.status;
     }
 
-    public SortBTreeNode getRight(){
-        return this.right;
+    /**
+     *
+     * @return
+     */
+    public boolean isArrival() {
+        return arrival;
     }
 
-    public SortBTreeNode getLeft() {
-        return left;
+    /**
+     *
+     * @return
+     */
+    public String getLocation() {
+        return location;
     }
 
-    void setRight(SortBTreeNode right) {
-        this.right = right;
+    /**
+     *
+     * @param status
+     */
+    public void setStatus(String status) {
+        this.location = location;
     }
 
-    public void setLeft(SortBTreeNode left) {
-        this.left = left;
+    /**
+     *
+     * @return
+     */
+    public Date getDate(){
+        return this.date;
     }
 
-    public void setItem(E item) {
-        this.item = item;
+    /**
+     *
+     * @param date
+     */
+    public void setDate(Date date){
+        this.date = date;
     }
 
-    boolean isLeaf(){
-        if ((right == null) && (left == null)){
-            return true;
+    /**
+     *
+     * @return
+     */
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    /**
+     *
+     */
+    public int compareTo(Flight other) {
+        if (isArrival() && other.isArrival()){
+            //check date
+            int compare = location.compareTo(other.getLocation());
+            if (compare == 0) { //sort by flight name instead
+                return date.compareTo(other.getDate());
+            } else {
+                return compare;
+            }
+        } else {
+            return -2; // its not valid to check
         }
-        return false;
+
     }
 }
