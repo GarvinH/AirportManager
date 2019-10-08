de37a2324121bb682e4a385c8a0ba682c296acb6
GarvinH <garvinh2001@hotmail.com>
2019-10-08 13:23:21 -0400

    Delete type-search-index.js

--- type-search-index.js
+++
@@ -1,1 +0,0 @@
-typeSearchIndex = [{"p":"<Unnamed>","l":"AirportManager"},{"l":"All Classes","url":"allclasses-index.html"},{"p":"<Unnamed>","l":"Flight"},{"p":"<Unnamed>","l":"PriorityQueue"},{"p":"<Unnamed>","l":"SortBTree"},{"p":"<Unnamed>","l":"Stack"}]
